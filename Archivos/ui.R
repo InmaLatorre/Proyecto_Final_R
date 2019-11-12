b17e86c84e94ef6ac8aa19dd56fa2d352e6976e4
Inma Latorre <ilatorre@MacBook-Pro-de-Julio.local>
2019-11-12 12:08:41 +0100

    Instrucciones y primer borrador de RMarkdown

---
+++ Archivos/ui.R
@@ -0,0 +1,13 @@
+pageWithSidebar(
+  headerPanel('Agrupamiento de K-medias de Iris'),
+  sidebarPanel(
+    selectInput('xcol', 'Variable X', names(iris)),
+    selectInput('ycol', 'Variable Y', names(iris),
+                selected=names(iris)[[2]]),
+    numericInput('clusters', 'Cuenta Clusters', 3,
+                 min = 1, max = 5)
+  ),
+  mainPanel(
+    plotOutput('plot1')
+  )
+)
